36293e697ea8b6adbb4abb6ee6e56517c8c1eb6c
DESKTOP-NB3NMRM\vlrvn <yanapos20@gmail.com>
2022-01-25 16:37:32 +0400

    new project

---
+++ First/First/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+namespace test73
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Номер тикита:");
+            string? name = Console.ReadLine();
+            Console.WriteLine($"build {name}");
+        }
+    }
+}
